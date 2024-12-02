2bf4db36d892dd1fa99389d14144b285c4c07833
guis28 <00001089601979sp@al.educacao.sp.gov.br>
2024-12-02 12:21:15 -0300

    Edit Files

--- perguntas.js
+++ perguntas.js
@@ -1,13 +1,13 @@
 criaCartao(
     'Sociologia',
     'Qual a nacionalidade do sociólogo e filósofo Durkheim?',
-    'Francesa. Nasceu na cidade francesa de Épinal, em 15 de abril de 1858, e faleceu em Paris, em 15 de novembro de 1917'
+    'Francesa. Nasceu na cidade francesa de Épinal, em 15 de abril de 1858, e faleceu em Paris, em 15 de novembro de 1917',
 )
 
 criaCartao(
     'História',
     'Quem inventou a lâmpada?',
-    'Thomas Edison (1847-1931). Foi inventada em 1879. No dia 21 de outubro, ele conseguiu manter uma lâmpada acessa durante 48 horas'
+    'Thomas Edison (1847-1931). Foi inventada em 1879. No dia 21 de outubro, ele conseguiu manter uma lâmpada acessa durante 48 horas',
 )
 
 criaCartao(
@@ -19,5 +19,5 @@ criaCartao(
 criaCartao(
     'Biologia',
     'Quantos ossos temos no nosso corpo?',
-    'O corpo humano tem 206 ossos: 22 na cabeça, 1 no pescoço, 6 no ouvido, 44 no tórax, 7 no abdômen, 62 nos membros inferiores e 64 nos membros inferiores'
+    'O corpo humano tem 206 ossos: 22 na cabeça, 1 no pescoço, 6 no ouvido, 44 no tórax, 7 no abdômen, 62 nos membros inferiores e 64 nos membros inferiores',
 )
