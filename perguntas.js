398fc590afcc1c3b29c70c7c90533558cfadc367
guis28 <00001089601979sp@al.educacao.sp.gov.br>
2024-12-02 11:59:21 -0300

    Edit Files

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,24 @@
 criaCartao(
     'Sociologia',
     'Qual a nacionalidade do sociólogo e filósofo Durkheim?',
-    'Francesa. Nasceu na cidade francesa de Épinal, em 15 de abril de 1858, e faleceu em Paris, em 15 de novembro de 1917.',
+    'Francesa. Nasceu na cidade francesa de Épinal, em 15 de abril de 1858, e faleceu em Paris, em 15 de novembro de 1917',
 )
 
 criaCartao(
     'História',
     'Quem inventou a lâmpada?',
-    'Thomas Edison (1847-1931). Foi inventada em 1879. No dia 21 de outubro, ele conseguiu manter uma lâmpada acessa durante 48 horas.',
+    'Thomas Edison (1847-1931). Foi inventada em 1879. No dia 21 de outubro, ele conseguiu manter uma lâmpada acessa durante 48 horas',
 )
 
 criaCartao(
     'Psicologia',
     'Qual é a diferença entre Ceticismo e Negação?',
-    'Ceticismo é a dúvida saudável diante da falta de evidencias.',
-    'Negação é a dúvida intencional diante da esmagadora quantidade de evidências confiáveis.',
+    'Ceticismo é a dúvida saudável diante da falta de evidencias',
+    'Negação é a dúvida intencional diante da esmagadora quantidade de evidências confiáveis',
 )
 
 criaCartao(
     'Biologia',
     'Quantos ossos temos no nosso corpo?',
-    'O corpo humano tem 206 ossos: 22 na cabeça, 1 no pescoço, 6 no ouvido, 44 no tórax, 7 no abdômen, 62 nos membros inferiores e 64 nos membros inferiores.',
+    'O corpo humano tem 206 ossos: 22 na cabeça, 1 no pescoço, 6 no ouvido, 44 no tórax, 7 no abdômen, 62 nos membros inferiores e 64 nos membros inferiores',
 )
